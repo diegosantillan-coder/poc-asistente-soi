8d4674a4a6efcc6d128b06a7bfa7b81a763229a2
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-08-21 18:16:30 -0500

    feat: definir rutas del app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'login',
+		loadComponent: async () =>
+			import('./components/login/login.component').then((m) => m.LoginComponent)
+	},
+	{
+		path: 'home',
+		loadComponent: async () =>
+			import('./components/home/home.component').then((m) => m.HomeComponent)
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'login'
+	}
+];
