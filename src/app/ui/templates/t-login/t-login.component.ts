185b7303b1b6f52754da8a8aa9ab4a4b0878998a
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-08-22 11:27:46 -0500

    feat: maquetacion login desktop

--- src/app/ui/templates/t-login/t-login.component.ts
+++ src/app/ui/templates/t-login/t-login.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+	FormBuilder,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { AtomsModule } from '@ui/atoms/atoms.module';
 import { ASeparatorComponent } from '../../atoms/a-separator/a-separator.component';
 
 @Component({
 	selector: 't-login',
 	standalone: true,
-	imports: [AtomsModule, ASeparatorComponent],
+	imports: [AtomsModule, ASeparatorComponent, ReactiveFormsModule],
 	templateUrl: './t-login.component.html',
 	styleUrl: './t-login.component.scss'
 })
-export class TLoginComponent {
+export class TLoginComponent implements OnInit {
+	loginForm!: FormGroup;
+	constructor(
+		private fb: FormBuilder,
+		private router: Router
+	) {}
+
+	ngOnInit(): void {
+		this.loginForm = this.fb.group({
+			documentType: ['', Validators.required],
+			documentNumber: [
+				'',
+				[Validators.required, Validators.pattern('^[0-9]+$')]
+			],
+			password: ['', [Validators.required, Validators.minLength(6)]]
+		});
+
+		this.loginForm.controls['documentType'].setValue('cc');
+	}
+
+	onSubmit(): void {
+		if (this.loginForm.valid) {
+			console.log(this.loginForm.value);
+			this.router.navigate(['/home']);
+		}
+	}
+
 	togglePasswordVisibility(): void {
 		const passwordField = document.getElementById(
 			'password'
